92c5aa2985506952a3a53fa01f0f10d1d2164296
Alisson Youssf <youssfbr@gmail.com>
2018-09-04 14:54:02 -0300

    Data criada, formatada e testada UTC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Date d = Date.from(Instant.parse("2018-09-04T14:50:07Z"));
+		
+		System.out.println(d);
+		System.out.println(sdf.format(d));
 	}
 
 }
